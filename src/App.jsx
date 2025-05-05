dbc298398e3c5095ea12e52117cacc9860098e96
shsobur <shsoburhossen951@gmail.com>
2025-05-05 15:54:27 +0600

    Basic mobile menu design completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ import Home from "./components/Home/Home";
 import { useRef } from "react";
 import { useEffect } from "react";
 import { HiMiniUserCircle } from "react-icons/hi2";
-import { AiOutlineAlipay } from "react-icons/ai";
+import { AiOutlineAlipay, AiOutlineShopping } from "react-icons/ai";
+import { IoHomeOutline } from "react-icons/io5";
+import { MdHistoryToggleOff, MdOutlineSpaceDashboard } from "react-icons/md";
+import { RiBloggerLine, RiContactsBook2Line } from "react-icons/ri";
+import { FiMessageCircle } from "react-icons/fi";
+import { CgProfile } from "react-icons/cg";
+import { PiSignIn } from "react-icons/pi";
 
 function App() {
   const [isScrollingDown, setIsScrollingDown] = useState(null);
@@ -13,7 +19,7 @@ function App() {
   const [open, setOpen] = useState(false);
   const menuRef = useRef();
 
-  // Handle close dropdown if clicked outside__
+  // Handle close dropdown__
   useEffect(() => {
     const handleClickOutside = (e) => {
       if (menuRef.current && !menuRef.current.contains(e.target)) {
@@ -25,7 +31,6 @@ function App() {
   }, []);
 
   // Handle Scroll__
-
   useEffect(() => {
     let lastScrollTop =
       window.pageYOffset || document.documentElement.scrollTop;
@@ -50,7 +55,11 @@ function App() {
   return (
     <>
       <nav>
-        <div id={isScrollingDown ? "navbar_close" : "navbar_open" } className="main_navbar_container">
+        {/* Desktop layout */}
+        <div
+          id={isScrollingDown ? "navbar_close" : "navbar_open"}
+          className="main_navbar_container"
+        >
           <div className="navbar_content_container">
             <h1 className="navbar_logo">
               <AiOutlineAlipay />
@@ -63,7 +72,6 @@ function App() {
               <li>Blogs</li>
               <li>About Us</li>
               <li>Contact Us</li>
-              <li>{isScrollingDown ? "True" : "False"}</li>
             </ul>
 
             <div className="dropdown_wrapper" ref={menuRef}>
@@ -75,12 +83,54 @@ function App() {
               </button>
               <div className={`dropdown_menu ${open ? "open" : ""}`}>
                 <a href="#" className="dropdown_item">
+                  <MdOutlineSpaceDashboard />
                   Dashboard
                 </a>
                 <a href="#" className="dropdown_item">
-                  Sign Out
+                  <PiSignIn /> Sign In
                 </a>
               </div>
+            </div>
+          </div>
+        </div>
+        {/* Mobile layout */}
+
+        <div className="main_mobile_menu_container">
+          <div className="mobile_menu_routes">
+            <ul>
+              <li>
+                <IoHomeOutline /> _Home
+              </li>
+              <li>
+                <AiOutlineShopping /> _Shop
+              </li>
+              <li>
+                <MdHistoryToggleOff /> _Order
+              </li>
+              <li>
+                <RiBloggerLine /> _Blogs
+              </li>
+              <li>
+                <FiMessageCircle /> _About Us
+              </li>
+              <li>
+                <RiContactsBook2Line /> _Contact Us
+              </li>
+            </ul>
+
+            <div className="others_routes_container">
+              <ul>
+                <li>
+                  <CgProfile /> Profile
+                </li>
+                <li>
+                  <MdOutlineSpaceDashboard /> Dashboard
+                </li>
+                <li>
+                  <PiSignIn />
+                  Sign In
+                </li>
+              </ul>
             </div>
           </div>
         </div>
